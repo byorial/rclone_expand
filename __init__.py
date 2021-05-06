2007f71794a356f5933f438d02e62695c506a1d2
soju6jan <cybersol@naver.com>
2021-05-06 16:31:11 +0900

    1

--- __init__.py
+++ __init__.py
@@ -1,1 +1,13 @@
+import os, sys
+from framework import app
+
+try:
+    from googleapiclient.discovery import build
+except ImportError:
+    try:
+        os.system("{} install google-api-python-client".format(app.config['config']['pip']))
+    except:
+        pass
+
+
 from .plugin import blueprint, menu, plugin_load, plugin_unload, plugin_info, process_telegram_data
